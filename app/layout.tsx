847040cfea9e3642b457f7821b26ff61c0c36426
MujtabaJ30 <86877651+MujtabaJ30@users.noreply.github.com>
2026-09-14 01:16:22 +0530

    redesign: remove modal runtime

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ProjectModalProvider } from "@/app/components/ProjectModalProvider";
 import { Header } from "@/app/components/Header";
 
 const geistSans = Geist({
@@ -30,10 +29,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} font-sans antialiased`}
       >
-        <ProjectModalProvider>
-          <Header />
-          {children}
-        </ProjectModalProvider>
+        <Header />
+        {children}
       </body>
     </html>
   );
